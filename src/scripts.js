c87ca06b8c24940a3e915483c800060006a6ccd9
Pedro Victor Castanheiro de Souza <71782584+PedroVictor002@users.noreply.github.com>
2022-09-04 20:55:04 -0300

    peraaaaa

---
+++ src/scripts.js
@@ -0,0 +1,15 @@
+let corsite = 'rosinha';
+function resetacor (cor){
+    corsite = cor;
+
+};
+console.log(corsite);
+resetacor("vermelho");
+console;log(corsite); let corsite = 'rosinha';
+function resetacor (cor){
+    corsite = cor;
+
+};
+console.log(corsite);
+resetacor("vermelho");
+console;log(corsite); 
